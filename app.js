7e58caccee360c0511056a81129193e933fe3e62
SiangYin <siangyin916@gmail.com>
2023-06-26 03:07:53 +0800

    addReview route

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ app.use(express.static("./public"));
 // to parse the body from post/fetch request
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 // ROUTES
 const authRouter = require("./routes/authRouter");
 const restaurantRouter = require("./routes/restaurantRouter");
+const reviewRouter = require("./routes/reviewRouter");
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/restaurant", restaurantRouter);
+app.use("/api/v1/review", reviewRouter);
 
-app.get("/", (req, res) => {
+app.get("/index.html", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
