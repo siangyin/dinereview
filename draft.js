e949e28d02c00092bb1e2320e8110b3cf63c9172
SiangYin <siangyin916@gmail.com>
2023-07-18 18:41:06 +0800

    remove env and add class for subnav

--- draft.js
+++
@@ -1,7 +0,0 @@
-// PORT=3000
-// DB_HOST=bacxkytfwb0xkde050ba-mysql.services.clever-cloud.com
-// DB_USERNAME=ulxxkcdldmgk4nt6
-// DB_PASSWORD=I3eKbtuG6crI06MQqpIw
-// DB_DBNAME=bacxkytfwb0xkde050ba
-// JWT_SECRET=a1749172210f625ccd39f7a30442bf599856d5dc98c1ac6db73571738aeff353
-// JWT_LIFETIME=30d
